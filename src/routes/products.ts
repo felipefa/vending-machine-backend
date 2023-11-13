aaa1811c2b1b68cedb2166232f0a9622f7bdd8ac
Felipe Araujo <devaraujofelipe@gmail.com>
2023-11-13 18:16:11 +0100

    feat: add method to update products

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -85,4 +85,94 @@ export const products = async (app: FastifyInstance) => {
         .send({ message: 'Failed to get products', error });
     }
   });
+
+  app.patch(
+    '/products/:id',
+    { preHandler: validateUserAuthorisation },
+    async (request: FastifyRequestWithUser, reply: FastifyReply) => {
+      try {
+        const paramsSchema = z.object({
+          id: z.string(),
+        });
+
+        const params = paramsSchema.parse(request.params);
+
+        if (!params.id) {
+          return reply.status(400).send({ message: 'Missing product ID' });
+        }
+
+        const bodySchema = z.object({
+          amountAvailable: z.number().optional(),
+          cost: z.number().optional(),
+          productName: z.string().optional(),
+        });
+
+        const { amountAvailable, cost, productName } = bodySchema.parse(
+          request.body
+        );
+
+        if (!amountAvailable && !cost && !productName) {
+          return reply
+            .status(400)
+            .send({ message: 'Missing product update data' });
+        }
+
+        const productId = params.id;
+        const sellerId = request.userId;
+
+        if (!sellerId) {
+          return reply.status(400).send({ message: 'Missing seller ID' });
+        }
+
+        const seller = (
+          await firestore.collection('users').doc(sellerId).get()
+        ).data();
+
+        if (seller?.role !== 'seller') {
+          return reply.status(401).send({ message: 'User is not a seller' });
+        }
+
+        const productDoc = await firestore
+          .collection('products')
+          .doc(productId)
+          .get();
+
+        if (!productDoc.exists) {
+          return reply.status(404).send({ message: 'Product not found' });
+        } else if (productDoc.data()?.sellerId !== sellerId) {
+          return reply
+            .status(403)
+            .send({ message: `Product doesn't belong to seller` });
+        } else {
+          const updatedProperties = {
+            ...(amountAvailable && { amountAvailable }),
+            ...(cost && { cost }),
+            ...(productName && { productName }),
+          };
+
+          await firestore
+            .collection('products')
+            .doc(productId)
+            .set(updatedProperties, { merge: true });
+
+          const product = {
+            amountAvailable,
+            cost,
+            id: productId,
+            productName,
+            sellerId,
+          } as Product;
+
+          return reply
+            .status(200)
+            .send({ message: 'Product updated successfully', product });
+        }
+      } catch (error) {
+        return reply.status(500).send({
+          message: 'Failed to update product',
+          error: (error as Error).message,
+        });
+      }
+    }
+  );
 };
