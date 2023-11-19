ffdf577585cc24a2f8ed1a8e18f69cfaf601d7c6
Felipe Araujo <devaraujofelipe@gmail.com>
2023-11-19 20:52:51 +0100

    refactor: rewrite tests for /users endpoint

--- src/routes/users.test.ts
+++ src/routes/users.test.ts
@@ -1,34 +1,56 @@
 import 'dotenv/config';
+
+import fastify, { FastifyInstance } from 'fastify';
+import { auth } from 'firebase-admin';
 import { UserRecord } from 'firebase-admin/lib/auth/user-record';
 
 import { firebaseAdmin, firestore } from '../lib/firebase';
+import { users } from './users';
 
-import { server as fastifyApp } from '../server';
+describe('users', () => {
+  let app: FastifyInstance;
 
-describe('createAccount', () => {
-  afterAll(async () => await fastifyApp.close());
+  beforeAll(async () => {
+    app = fastify();
+
+    app.register(users);
+
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
   afterEach(jest.resetAllMocks);
 
-  it('should create a new account', async () => {
-    jest
-      .spyOn(firebaseAdmin.auth(), 'createUser')
-      .mockImplementation(async () => {
-        return {
-          uid: 'uuid',
-        } as UserRecord;
-      });
+  it('should create a new user', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'createUser').mockResolvedValue({
+      uid: 'mockUserId',
+    } as UserRecord);
 
-    jest
-      .spyOn(firestore.collection('users').doc('uuid'), 'set')
-      .mockImplementation(async () => {
-        return {} as FirebaseFirestore.WriteResult;
-      });
+    const get = jest.fn(async () => ({
+      empty: true,
+    }));
+    const create = jest.fn(async () => {});
+    const where = jest.fn(() => ({
+      get,
+    }));
+    const doc = jest.fn(() => ({
+      get,
+      create,
+    }));
 
-    const response = await fastifyApp.inject({
+    jest.spyOn(firestore, 'collection').mockReturnValue({
+      doc,
+      where,
+    } as unknown as any);
+
+    const response = await app.inject({
       method: 'POST',
       url: '/users',
       payload: {
-        email: 'test@email.com',
+        email: 'test@example.com',
         password: 'password',
         role: 'buyer',
         username: 'testuser',
@@ -42,75 +64,189 @@ describe('createAccount', () => {
     );
   });
 
-  it('should fail when required fields are missing', async () => {
-    const response = await fastifyApp.inject({
+  it('should return an error if username already exists', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'createUser').mockResolvedValue({
+      uid: 'mockUserId',
+    } as UserRecord);
+
+    const get = jest.fn(async () => ({
+      empty: false,
+      data: () => ({
+        username: 'existingUser',
+      }),
+    }));
+    const where = jest.fn(() => ({
+      get,
+    }));
+    const doc = jest.fn(() => ({
+      get,
+    }));
+
+    jest
+      .spyOn(firestore, 'collection')
+      .mockReturnValue({ doc, where } as unknown as any);
+
+    const response = await app.inject({
       method: 'POST',
       url: '/users',
       payload: {
         email: 'test@example.com',
         password: 'password',
-        // role and username are missing
+        role: 'buyer',
+        username: 'existingUser',
       },
     });
 
-    expect(response.statusCode).toBe(500);
-    expect(response.json()).toHaveProperty(
-      'message',
-      'Account creation failed'
+    expect(response.statusCode).toBe(400);
+    expect(response.json()).toEqual({
+      message: 'Account creation failed',
+      error: 'Username already exists',
+    });
+  });
+
+  it('should get a user by ID', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'verifyIdToken').mockReturnValue(
+      (async () =>
+        ({
+          uid: 'mockUserId',
+        } as auth.DecodedIdToken))()
+    );
+
+    jest.spyOn(firestore, 'collection').mockReturnValue({
+      doc: jest.fn(() => ({
+        get: jest.fn().mockResolvedValue({
+          exists: true,
+          data: () => ({
+            id: 'mockUserId',
+            username: 'testuser',
+          }),
+        }),
+      })),
+    } as unknown as any);
+
+    const response = await app.inject({
+      headers: { authorization: 'Bearer userIdToken' },
+      method: 'GET',
+      url: '/users/mockUserId',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toEqual({
+      message: 'User found',
+      user: {
+        id: 'mockUserId',
+        username: 'testuser',
+      },
+    });
+  });
+
+  it('should return an error if user is not found', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'verifyIdToken').mockReturnValue(
+      (async () =>
+        ({
+          uid: 'mockUserId',
+        } as auth.DecodedIdToken))()
+    );
+
+    jest.spyOn(firestore, 'collection').mockReturnValue({
+      doc: jest.fn(() => ({
+        get: jest.fn().mockResolvedValue({
+          exists: false,
+        }),
+      })),
+    } as unknown as any);
+
+    const response = await app.inject({
+      headers: { authorization: 'Bearer userIdToken' },
+      method: 'GET',
+      url: '/users/nonExistentUserId',
+    });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.json()).toEqual({
+      message: 'User not found',
+    });
+  });
+
+  it('should update a user', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'verifyIdToken').mockReturnValue(
+      (async () =>
+        ({
+          uid: 'mockUserId',
+        } as auth.DecodedIdToken))()
+    );
+
+    const set = jest.fn(async () => {});
+
+    jest.spyOn(firestore, 'collection').mockReturnValue({
+      doc: jest.fn(() => ({
+        set,
+      })),
+    } as unknown as any);
+
+    const response = await app.inject({
+      headers: { authorization: 'Bearer userIdToken' },
+      method: 'PATCH',
+      url: '/users',
+      payload: {
+        username: 'newusername',
+      },
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toEqual({
+      message: 'User updated successfully',
+    });
+    expect(set).toHaveBeenCalledWith(
+      { username: 'newusername' },
+      { merge: true }
     );
   });
 
-  it('should fail when Firebase admin operation fails', async () => {
-    jest.spyOn(firebaseAdmin.auth(), 'createUser').mockImplementation(() => {
-      throw new Error('Firebase admin operation failed');
-    });
+  it('should delete a user', async () => {
+    jest.spyOn(firebaseAdmin.auth(), 'verifyIdToken').mockReturnValue(
+      (async () =>
+        ({
+          uid: 'mockUserId',
+        } as auth.DecodedIdToken))()
+    );
 
-    const response = await fastifyApp.inject({
-      method: 'POST',
+    const deleteFn = jest.fn(async () => {});
+    const batchCommit = jest.fn(async () => {});
+
+    jest.spyOn(firestore, 'collection').mockReturnValue({
+      doc: jest.fn(() => ({
+        delete: deleteFn,
+      })),
+      where: jest.fn(() => ({
+        get: jest.fn().mockResolvedValue({
+          docs: [
+            {
+              ref: {
+                delete: deleteFn,
+              },
+            },
+          ],
+        }),
+      })),
+    } as unknown as any);
+
+    jest.spyOn(firestore, 'batch').mockReturnValue({
+      delete: deleteFn,
+      commit: batchCommit,
+    } as unknown as any);
+
+    const response = await app.inject({
+      headers: { authorization: 'Bearer userIdToken' },
+      method: 'DELETE',
       url: '/users',
-      payload: {
-        email: 'test@example.com',
-        password: 'password',
-        role: 'buyer',
-        username: 'testuser',
-      },
     });
 
-    expect(response.statusCode).toBe(500);
+    expect(response.statusCode).toBe(200);
     expect(response.json()).toEqual({
-      message: 'Account creation failed',
-      error: 'Firebase admin operation failed',
+      message: 'User deleted',
     });
-  });
-
-  it('should fail when Firestore operation fails', async () => {
-    jest
-      .spyOn(firebaseAdmin.auth(), 'createUser')
-      .mockImplementation(async () => {
-        return {
-          uid: 'uuid',
-        } as UserRecord;
-      });
-
-    jest.spyOn(firestore, 'collection').mockImplementation(() => {
-      throw new Error('Firestore operation failed');
-    });
-
-    const response = await fastifyApp.inject({
-      method: 'POST',
-      url: '/users',
-      payload: {
-        email: 'test@example.com',
-        password: 'password',
-        role: 'buyer',
-        username: 'testuser',
-      },
-    });
-
-    expect(response.statusCode).toBe(500);
-    expect(response.json()).toEqual({
-      message: 'Account creation failed',
-      error: 'Firestore operation failed',
-    });
+    expect(deleteFn).toHaveBeenCalledTimes(2);
+    expect(batchCommit).toHaveBeenCalled();
   });
 });
